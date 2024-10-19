3c6c0d79d27eee88a6cdcc9c82b1af87e16910e6
napoleonjr <146306013+napoleonjr@users.noreply.github.com>
2024-10-19 12:35:22 +0800

    Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,22 +14,8 @@ const Footer = () => {
           <TbBrandGithub />
         </span>
       </a>
-      <a
-        href="https://www.youtube.com/channel/UChkOsij0dhgft0GhHRauOAA"
-        target="_blank"
-      >
-        <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
-          <SlSocialYoutube />
-        </span>
-      </a>
-      <a
-        href="https://www.linkedin.com/in/noor-mohammad-ab2245193/"
-        target="_blank"
-      >
-        <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
-          <SlSocialLinkedin />
-        </span>
-      </a>
+      
+      
       <a href="https://www.facebook.com/Noorlalu143/" target="_blank">
         <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
           <SlSocialFacebook />
